2209b8f63bd3aa6c9abd2958fb4761710a10bf6c
viniciusbenicio <vinicius.benicio97@gmail.com>
2023-03-20 19:47:46 -0300

    Commit

--- Program.cs
+++ Program.cs
@@ -6,20 +6,26 @@ namespace Calculadora
     {
         static void Main(string[] args)
         {
-            
-            Console.WriteLine("Primeiro valor: ");
-            float n1 =  float.Parse(Console.ReadLine());
 
+            Somar();
+
+        }
+
+        static void Somar()
+        {
+            // Primeiro valor para realizar a soma.
+            Console.WriteLine("Primeiro valor: ");
+            float n1 = float.Parse(Console.ReadLine());
+
+            // Segundo valor para realizar a soma.
             Console.WriteLine("Segundo valor: ");
             float n2 = float.Parse(Console.ReadLine());
 
-
+            //Resultado da soma dos dois valores n1 e n2.
             float resultado = n1 + n2;
 
+            // Mostrando no Console uma string com o resultado.
             Console.WriteLine($"Resultado da Soma: {resultado} ");
-
-
-
         }
     }
 }
