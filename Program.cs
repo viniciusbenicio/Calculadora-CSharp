802801a41f2d03288c6c336ac0263109813a12ae
viniciusbenicio <vinicius.benicio97@gmail.com>
2023-03-20 19:42:55 -0300

    Somar valores

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Primeiro valor: ");
+            float n1 =  float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor: ");
+            float n2 = float.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Resultado da Soma: ");
+            Console.WriteLine(n1 + n2);
+
+
+
+        }
+    }
+}
